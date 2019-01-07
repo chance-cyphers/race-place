147097bbd3ffa02ff449b0d9c7e2d4477e77c819
chance <chance.a.cyphers@gmail.com>
2019-01-07 12:41:56 -0500

    First

---
+++ android/app/src/main/kotlin/com/pants/chance/raceplace/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.pants.chance.raceplace
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
