eff17faf501fd927fac3d18c936c28686cc36b04
chance <chance.a.cyphers@gmail.com>
2019-01-09 16:47:41 -0500

    Setup interval functions for lobby

--- lib/lobby_page.dart
+++ lib/lobby_page.dart
@@ -1,23 +1,40 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:race_place/entrant.dart';
 
-class LobbyPage extends StatelessWidget {
+
+class LobbyPage extends StatefulWidget {
   LobbyPage({Key key, @required this.entrant}) : super(key: key);
 
   final Entrant entrant;
 
   @override
+  _LobbyPageState createState() => _LobbyPageState();
+}
+
+class _LobbyPageState extends State<LobbyPage> {
+
+  @override
+  void initState() {
+    super.initState();
+    Timer.periodic(new Duration(seconds: 2), (timer) {
+      print("asd");
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Race place"),
+        title: Text("Race placezzz"),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
             Text(
-              'Finding match for ${entrant.userId}...',
+              'Finding match for ${widget.entrant.userId}...',
             ),
           ],
         ),
