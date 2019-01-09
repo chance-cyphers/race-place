e4e6936d0acb61029abd99985335f767a5b8dd16
chance <chance.a.cyphers@gmail.com>
2019-01-09 17:31:43 -0500

    Navigate to race plage when race starts
    Currently has bug where at least two pages are navigated to since we dont properly dispose of the timer

--- lib/lobby_page.dart
+++ lib/lobby_page.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:race_place/entrant.dart';
 import 'package:race_place/race_api_client.dart';
 
-
 class LobbyPage extends StatefulWidget {
   LobbyPage({Key key, @required this.entrant}) : super(key: key);
 
@@ -15,25 +14,34 @@ class LobbyPage extends StatefulWidget {
 }
 
 class _LobbyPageState extends State<LobbyPage> {
+  Timer timer;
 
   @override
   void initState() {
     super.initState();
-    Timer.periodic(new Duration(seconds: 2), (timer) {
-      raceApiClient.getTrack(widget.entrant.links.track)
-        .then((track) {
-          print(track.status);
-          print(track.entrants[0].userId);
-          print(track.entrants[0].distance);
+    timer = Timer.periodic(new Duration(seconds: 2), (timer) {
+      raceApiClient.getTrack(widget.entrant.links.track).then((track) {
+        if (track.status == "started") {
+          Navigator.of(context).push(new MaterialPageRoute(
+              maintainState: false,
+              builder: (BuildContext buildContext) => RacePage()));
+          timer.cancel();
+        }
       });
     });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    timer.cancel();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Race placezzz"),
+        title: Text("Race place"),
       ),
       body: Center(
         child: Column(
@@ -41,6 +49,27 @@ class _LobbyPageState extends State<LobbyPage> {
           children: <Widget>[
             Text(
               'Finding match for ${widget.entrant.userId}...',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RacePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Race place"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text(
+              'You are racing now. Just so you know.',
             ),
           ],
         ),
