07b1dde0a64da4ee205a1cc8fbca5caf1c974fdf
chance <chance.a.cyphers@gmail.com>
2019-01-17 18:25:49 -0500

    Print loc on race screen

--- lib/race_page.dart
+++ lib/race_page.dart
@@ -1,6 +1,39 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class RacePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+
+class RacePage extends StatefulWidget {
+  @override
+  _RacePageState createState() => _RacePageState();
+}
+
+class _RacePageState extends State<RacePage> {
+
+  StreamSubscription<Map<String, double>> _locationSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+
+    var location = new Location();
+
+    this._locationSubscription = location.onLocationChanged().listen((Map<String,double> currentLocation) {
+      print(currentLocation["latitude"]);
+      print(currentLocation["longitude"]);
+      print(currentLocation["accuracy"]);
+      print(currentLocation["altitude"]);
+      print(currentLocation["speed"]);
+      print(currentLocation["speed_accuracy"]); // Will always be 0 on iOS
+    });
+  }
+
+  @override
+  void deactivate() {
+    super.deactivate();
+    _locationSubscription.cancel();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
