c63176969066e5ebcbf024720f7e65f1126992d7
chance <chance.a.cyphers@gmail.com>
2019-01-23 17:09:59 -0500

    Minor refactor

--- lib/bloc/race_bloc.dart
+++ lib/bloc/race_bloc.dart
@@ -14,7 +14,6 @@ class RaceBloc {
   Timer _timer;
 
   Stream<RaceInfo> get model => _raceInfoController.stream;
-
   Stream<String> get winner => _winnerController.stream;
 
   RaceBloc(this._track) {
@@ -36,20 +35,21 @@ class RaceBloc {
     });
 
     _timer = Timer.periodic(new Duration(seconds: 3), (timer) {
-      raceApiClient.getTrack(_track.links.self).then((track) {
-        var entrant1 = track.entrants[0];
-        var entrant2 = track.entrants[1];
-        var label1 = entrant1.userId + ": " + entrant1.distance.toString();
-        var label2 = entrant2.userId + ": " + entrant2.distance.toString();
-        var model = RaceInfo(
-            label1, label2, entrant1.distance, entrant2.distance);
-        _raceInfoController.add(model);
-
-        if (track.winner != null && track.winner != "") {
-          _winnerController.add(track.winner);
-        }
-      });
+      raceApiClient.getTrack(_track.links.self).then(_handleNewTrack);
     });
+  }
+
+  void _handleNewTrack(track) {
+    var entrant1 = track.entrants[0];
+    var entrant2 = track.entrants[1];
+    var label1 = entrant1.userId + ": " + entrant1.distance.toString();
+    var label2 = entrant2.userId + ": " + entrant2.distance.toString();
+    var model = RaceInfo(label1, label2, entrant1.distance, entrant2.distance);
+    _raceInfoController.add(model);
+
+    if (track.winner != null && track.winner != "") {
+      _winnerController.add(track.winner);
+    }
   }
 
   void close() {
