15db7626de27bd5a084cc90fa8a99cd3735ec3fe
chance <chance.a.cyphers@gmail.com>
2019-01-23 18:09:20 -0500

    Add textfield for whatever username

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -2,13 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:race_place/page/lobby_page.dart';
 import 'package:race_place/api/race_api_client.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() =>  _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  final _usernameController = TextEditingController();
 
   void _onPress(BuildContext context) {
-    raceApiClient.createEntrant("placeholderJohnson").then((entrant) {
+    var isEmpty = _usernameController.text.isEmpty || _usernameController.text == null;
+    var username = isEmpty
+        ? "Placeholder Johnson" : _usernameController.text;
+    raceApiClient.createEntrant(username).then((entrant) {
       Navigator.of(context).push(new MaterialPageRoute(
           builder: (BuildContext buildContext) => LobbyPage(entrant: entrant)));
     });
+  }
+
+  void dispose() {
+    _usernameController.dispose();
+    super.dispose();
   }
 
   @override
@@ -17,10 +32,15 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Race place"),
       ),
-      body: Center(
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 60),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
+            TextField(
+                controller: _usernameController,
+              decoration: InputDecoration(hintText: "Enter a username"),
+            ),
             Text(
               'Welcome to race place',
             ),
@@ -35,5 +55,7 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
+
+
 }
 
