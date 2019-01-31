a645855880254f0803b27b14fd6d19e8da411891
chance <chance.a.cyphers@gmail.com>
2019-01-31 18:04:46 -0500

    Nav to home page on login

--- lib/page/login_page.dart
+++ lib/page/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:race_place/auth/auth.dart';
 import 'package:race_place/auth/credentials_keeper.dart';
+import 'package:race_place/page/home_page.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -8,13 +9,23 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginState extends State<LoginPage> {
-
   void _login() {
     loginClient.login("test@test.com", "ChuckNorris1").then((creds) {
-      credentialsKeeper.save(creds);
+      return credentialsKeeper.save(creds);
+    }).then((_) {
+      _gotoHome();
     }).catchError((err) {
       print('Error saving credentials: ' + err.toString());
     });
+  }
+
+  void _gotoHome() {
+    Navigator.pushAndRemoveUntil(
+        context,
+        new MaterialPageRoute(
+            maintainState: false,
+            builder: (BuildContext buildContext) => HomePage()),
+        ModalRoute.withName("/"));
   }
 
   @override
