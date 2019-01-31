a1910d08fab0fd89121fbe2e4e2758407ae87a22
chance <chance.a.cyphers@gmail.com>
2019-01-31 17:58:18 -0500

    Check for fresh access for hasValidCreds

--- lib/auth/credentials_keeper.dart
+++ lib/auth/credentials_keeper.dart
@@ -4,32 +4,40 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 final CredentialsKeeper credentialsKeeper = CredentialsKeeper._private();
 
 class CredentialsKeeper {
-
   CredentialsKeeper._private();
+
   final storage = new FlutterSecureStorage();
 
-  // when we login
   Future<void> save(Creds creds) {
     return storage.write(key: "credentials", value: creds.toJson());
   }
 
-  // when we logout
   Future<void> clear() {
     return storage.deleteAll();
   }
 
-  // so we know to refresh or what
-  bool hasValidCreds() {
-    // check when access expires
-    // check if there be refreshments
+  Future<bool> hasValidCreds() async {
+    var creds = await _retrieveCredentials();
+
+    if (creds.accessToken == null) {
+      return false;
+    }
+
+    var expiration = Duration(minutes: creds.expiresIn);
+    if (expiration < Duration(minutes: 10)) {
+      return false;
+    }
+
     return false;
   }
 
-  // retrieve them if they're not expired. Or refresh
   Future<Creds> getCredentials() async {
+    return await _retrieveCredentials();
+  }
+
+  Future<Creds> _retrieveCredentials() {
     return storage.read(key: "credentials").then((json) {
       return Creds.fromJson(json);
     });
   }
-
 }
